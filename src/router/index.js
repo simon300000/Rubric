1d5c46714ae29425303a5d31df237db61267778c
soes64121 <soes64121@his-student.org>
2018-04-14 14:33:08 +0800

    Setup -2
    - Fixing setup errors

---
+++ src/router/index.js
@@ -0,0 +1,26 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import HelloWorld from '@/components/HelloWorld'
+import home from '@/components/home'
+import rubric from '@/components/rubric'
+
+Vue.use(Router)
+
+export default new Router({
+  mode: 'history',
+  routes: [{
+      path: '/',
+      name: 'HelloWorld',
+      component: HelloWorld
+    }, {
+      path: '/home',
+      name: 'home',
+      component: home
+    }, {
+      path: '/rubric',
+      name: 'rubric',
+      component: rubric
+    }
+  ],
+  linkExactActiveClass: 'active'
+})
